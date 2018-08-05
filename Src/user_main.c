fce70de0262359da95cb3ef6b4a54f9923c51617
Graeme Russ <graeme.russ@gmail.com>
2018-08-06 05:38:43 +1000

    Add copyright notices

--- Src/user_main.c
+++ Src/user_main.c
@@ -1,8 +1,19 @@
 /*
- * user_main.c
+ * HAX - Home Automation eXpansion
+ * Copyright (C) 2018  Graeme Russ <gdr@goatjam.net>
  *
- *  Created on: 29Jul.,2018
- *      Author: gruss
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "stm32f4xx_hal.h"
